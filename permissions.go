e397398bf2ad20a01df359b933e066ec3ce1dad0
Kayle Gishen <k@bkdsw.com>
2017-02-07 15:00:06 -0500

    Added binary marshalling

--- permissions.go
+++ permissions.go
@@ -2,10 +2,17 @@
 package goahead
 
 import (
+	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"strconv"
 
 	"github.com/willf/bitset"
+)
+
+var (
+	ErrBufTooSmall = errors.New("Binary buffer is too small")
+	ErrBufOveflow  = errors.New("64 bit overflow detected")
 )
 
 // PermissionSet represents a group of permissions where each set bit grants
@@ -190,7 +197,91 @@ func UnmarshalJSON(data []byte) (*PermissionSet, error) {
 	return set, nil
 }
 
-// Bytes returns the underling storag. The indices correspond to walking through
+func (p *PermissionSet) MarshalBinary() ([]byte, error) {
+	bytes, err := p.bits.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+	//[ID][SIZE][TOTAL_SIZE][DATA]{[CHILD_INDEX][CHILD_DATA]}
+	size := uint64(len(bytes))
+	data := make([]byte, 24+size)
+	binary.PutUvarint(data, p.ID)
+	binary.PutUvarint(data[8:], size)
+	copy(data[24:], bytes)
+
+	if p.children != nil {
+		for i, v := range p.children {
+			if b, err := v.MarshalBinary(); err != nil {
+				return nil, err
+			} else {
+				header := make([]byte, 8)
+				binary.PutUvarint(header, uint64(i))
+				size += 8 + uint64(len(b))
+				data = append(data, append(header, b...)...)
+			}
+		}
+	}
+	binary.PutUvarint(data[16:], size+24)
+
+	return data, nil
+}
+
+func (p *PermissionSet) UnmarshalBinary(data []byte) error {
+	maxLen := uint64(len(data))
+	if maxLen == 0 {
+		return nil
+	}
+
+	//[ID][SIZE][TOTAL_SIZE][DATA]{[CHILD_INDEX][CHILD_DATA]}
+	var read int
+	p.ID, read = binary.Uvarint(data[:7])
+	if read == 0 {
+		return ErrBufTooSmall
+	} else if read < 0 {
+		return ErrBufOveflow
+	}
+	size, read := binary.Uvarint(data[8:15])
+	if read == 0 {
+		return ErrBufTooSmall
+	} else if read < 0 {
+		return ErrBufOveflow
+	}
+	err := p.bits.UnmarshalBinary(data[24 : 24+size])
+	if err != nil {
+		return err
+	}
+
+	// Look for children
+	offset := 24 + size
+	if maxLen > offset {
+		p.children = make(map[uint]*PermissionSet)
+		for maxLen > offset {
+			//[IDX][ID][SIZE][TOTAL_SIZE]
+			//[0-7][8-15][16-23][24-31]
+			child := new(PermissionSet)
+			idx, read := binary.Uvarint(data[offset : offset+7])
+			if read == 0 {
+				return ErrBufTooSmall
+			} else if read < 0 {
+				return ErrBufOveflow
+			}
+			childSize, read := binary.Uvarint(data[offset+24 : offset+31])
+			if read == 0 {
+				return ErrBufTooSmall
+			} else if read < 0 {
+				return ErrBufOveflow
+			}
+			if err := child.UnmarshalBinary(data[offset+8 : offset+8+childSize]); err != nil {
+				return err
+			}
+			p.children[uint(idx)] = child
+			offset += 8 + childSize + 1
+		}
+	}
+	return nil
+}
+
+// Bytes returns the underling storage. The indices correspond to walking through
 // the children.
 func (p *PermissionSet) Bytes(indices ...uint) []uint64 {
 	var set *PermissionSet
